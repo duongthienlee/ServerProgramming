1c0190e1e120544f0aa1a9ad3d10f49ed119a3a4
duongthienly <duongthienlee@gmail.com>
2018-02-11 11:43:19 +0200

    Function Delete and Edit book added

--- src/main/java/fi/haagahelia/Bookstore/Bookstore/domain/BookRepository.java
+++ src/main/java/fi/haagahelia/Bookstore/Bookstore/domain/BookRepository.java
@@ -3,5 +3,6 @@ package fi.haagahelia.Bookstore.Bookstore.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-
+	
 }
+
--- src/main/java/fi/haagahelia/Bookstore/Bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/Bookstore/Bookstore/web/BookController.java
@@ -3,8 +3,9 @@ package fi.haagahelia.Bookstore.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import fi.haagahelia.Bookstore.Bookstore.domain.Book;
 import fi.haagahelia.Bookstore.Bookstore.domain.BookRepository;
@@ -20,8 +21,8 @@ public class BookController {
 		return "Listpage";
 	}
 
-	@RequestMapping("/create")
-	public String addBook(Model model) {
+	@RequestMapping("/add")
+	public String createBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addBook";
 	}
@@ -30,6 +31,20 @@ public class BookController {
 	public String saveBook(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.delete(bookId);
+		return "redirect:../booklist";
+
+	}
+
+	@RequestMapping("/edit{id}")
+	public String findBook(@PathVariable("id") Long bookid, Model model) {
+		model.addAttribute("book", repository.findOne(bookid));
+		return "editBook";
+
 	}
 
 }
