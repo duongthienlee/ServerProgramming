0ad621fac12d6b0445657b6ed802afc8788e93ed
duongthienly <duongthienlee@gmail.com>
2018-03-17 20:39:52 +0200

    Fix minor issues, revert back to the runnable version

--- src/main/java/fi/haagahelia/Bookstore/domain/CategoryRepository.java
+++ src/main/java/fi/haagahelia/Bookstore/domain/CategoryRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long>  {
 	List<Category> findByName(String name);
-	
 }
--- src/test/java/fi/haagahelia/Bookstore/Bookstore/BookstoreApplicationTests.java
+++ src/test/java/fi/haagahelia/Bookstore/Bookstore/BookstoreApplicationTests.java
@@ -1,25 +1,16 @@
 package fi.haagahelia.Bookstore.Bookstore;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import fi.haagahelia.Bookstore.web.BookController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
 
-	@Autowired
-	private BookController controller;
-	//Smoke testing
 	@Test
-	public void contexLoads() throws Exception {
-		assertThat(controller).isNotNull();
-		}
+	public void contextLoads() {
+	}
 
 }
